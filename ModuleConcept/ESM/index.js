c4d4a8844ff6a7ea51a3955619d784f6f64c5212
FazleyRM <frahaman520@gmail.com>
2026-07-21 21:40:16 +0530

    ESM Module Concept

---
+++ ModuleConcept/ESM/index.js
@@ -0,0 +1,18 @@
+import { add, substract, multiplication } from "./math.js";
+import { capitalize, reverse, countCharracter } from "./string.js";
+import { userDetails } from "./user.js";
+function main(){
+    add();
+    substract();
+    multiplication();
+}
+function main1(){
+    capitalize();
+    reverse();
+    countCharracter();
+
+}
+
+main();
+main1();
+
---
+++ ModuleConcept/ESM/math.js
@@ -0,0 +1,20 @@
+function add(){
+    let a = 10;
+    let b = 20;
+    let c = a + b;
+    console.log("Additioanl: "+ c );
+}
+function substract(){
+    let a = 20;
+    let b = 13;
+    let c = a-b;
+    console.log("Substract: " + c );
+}
+function multiplication(){
+    let a = 10;
+    let b = 12;
+    let c = a * b;
+    console.log("Multiplication: "+ c);
+
+}
+export{ add, multiplication, substract };
